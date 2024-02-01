52d63fbfdeb23ef5b055beed96f231b40bafe38a
Deepanshu Singhal <deepanshusinghal2003@gmail.com>
2024-02-01 11:49:54 +0530

    4th

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-   <div>
-    Ayush
-   </div>  );
-=======
     <div>
-      Deepanshu
+      BNB Traders
    </div>
   );
->>>>>>> 9b6bb5439bfddf5b9a954657ff75e12b699b50ba
 }
